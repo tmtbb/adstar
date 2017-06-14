bd6016c68dc7ff8b5b8d5133ec68b2ef6c8fe678
SL-Y <470707973@qq.com>
2017-06-15 00:38:30 +0800

    完成倒计时接口

--- app/src/main/java/com/yundian/star/ui/main/fragment/AuctionMarketFragment.java
+++ app/src/main/java/com/yundian/star/ui/main/fragment/AuctionMarketFragment.java
@@ -31,8 +31,6 @@ import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
-import java.lang.ref.WeakReference;
-
 import butterknife.Bind;
 
 import static com.yundian.star.R.id.rb_1;
@@ -46,15 +44,15 @@ import static com.yundian.star.R.id.rb_1;
 public class AuctionMarketFragment extends BaseFragment {
 
     @Bind(R.id.iv_src)
-    ImageView iv_src ;
+    ImageView iv_src;
     @Bind(R.id.tv_residue_time)
-    TextView tv_residue_time ;
+    TextView tv_residue_time;
     @Bind(R.id.tv_have_name)
-    TextView tv_have_name ;
+    TextView tv_have_name;
     @Bind(R.id.tv_have_time)
-    TextView tv_have_time ;
+    TextView tv_have_time;
     @Bind(R.id.seekBar)
-    MySeekBar seekBar ;
+    MySeekBar seekBar;
     @Bind(R.id.fl_auction_content)
     FrameLayout fl_auction_content;
     private AutionTopFragment aution_buy;
@@ -65,8 +63,7 @@ public class AuctionMarketFragment extends BaseFragment {
     private int userId;
     private String token;
     private CountDownTimer timer;
-    private MyHandler1 myHandler;
-    private int secondTime = 0 ;
+    private int secondTime = 0;
 
 
     @Override
@@ -81,7 +78,7 @@ public class AuctionMarketFragment extends BaseFragment {
 
     @Override
     protected void initView() {
-        if (getArguments()!=null){
+        if (getArguments() != null) {
             code = getArguments().getString(AppConstant.STAR_CODE);
         }
         if (flag) {
@@ -90,7 +87,7 @@ public class AuctionMarketFragment extends BaseFragment {
         }
         userId = SharePrefUtil.getInstance().getUserId();
         token = SharePrefUtil.getInstance().getToken();
-        myHandler = new MyHandler1(this);
+        //myHandler = new MyHandler1(this);
         LogUtils.loge("走一次了");
         initListener();
         //initData();
@@ -105,12 +102,13 @@ public class AuctionMarketFragment extends BaseFragment {
 
             @Override
             public void onSuccess(TradingStatusBeen tradingStatusBeen) {
-                if (tradingStatusBeen!=null){
-                    if (tradingStatusBeen.isStatus()){
+                if (tradingStatusBeen != null) {
+                    if (tradingStatusBeen.isStatus()) {
+                        startSunTime = true;
                         secondTime = tradingStatusBeen.getRemainingTime();
-                        myHandler.sendEmptyMessage(myHandler.GRT_DATA);
+                        myHandler.sendEmptyMessage(GRT_DATA);
                         //startTime(tradingStatusBeen.getRemainingTime());
-                    }else {
+                    } else {
                         tv_residue_time.setText("未开始");
                     }
                 }
@@ -127,20 +125,20 @@ public class AuctionMarketFragment extends BaseFragment {
 
     private void initFragment(Bundle savedInstanceState) {
         FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
-        if (savedInstanceState==null){
+        if (savedInstanceState == null) {
             Bundle bundle1 = new Bundle();
-            bundle1.putString(AppConstant.STAR_CODE,code);
-            bundle1.putInt(AppConstant.AUCTION_TYPE,1);
+            bundle1.putString(AppConstant.STAR_CODE, code);
+            bundle1.putInt(AppConstant.AUCTION_TYPE, 1);
             aution_buy = new AutionTopFragment();
             aution_buy.setArguments(bundle1);
-            Bundle bundle2= new Bundle();
-            bundle2.putString(AppConstant.STAR_CODE,code);
-            bundle2.putInt(AppConstant.AUCTION_TYPE,2);
+            Bundle bundle2 = new Bundle();
+            bundle2.putString(AppConstant.STAR_CODE, code);
+            bundle2.putInt(AppConstant.AUCTION_TYPE, 2);
             aution_shell = new AutionTopFragment();
             aution_shell.setArguments(bundle2);
-            transaction.add(R.id.fl_auction_content, aution_buy,"AutionBuy");
-            transaction.add(R.id.fl_auction_content, aution_shell,"AutionShell");
-        }else {
+            transaction.add(R.id.fl_auction_content, aution_buy, "AutionBuy");
+            transaction.add(R.id.fl_auction_content, aution_shell, "AutionShell");
+        } else {
             aution_buy = (AutionTopFragment) getChildFragmentManager().findFragmentByTag("AutionBuy");
             aution_shell = (AutionTopFragment) getChildFragmentManager().findFragmentByTag("AutionShell");
         }
@@ -148,17 +146,17 @@ public class AuctionMarketFragment extends BaseFragment {
     }
 
     private void initListener() {
-        RadioGroup radioGroup = (RadioGroup)rootView.findViewById(R.id.radio_group);
-        RadioButton button = (RadioButton)radioGroup.findViewById(R.id.rb_1);
+        RadioGroup radioGroup = (RadioGroup) rootView.findViewById(R.id.radio_group);
+        RadioButton button = (RadioButton) radioGroup.findViewById(R.id.rb_1);
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
 
-                switch (group.getId()){
-                    case rb_1 :
+                switch (group.getId()) {
+                    case rb_1:
                         SwitchTo(0);
                         break;
-                    case R.id.rb_2 :
+                    case R.id.rb_2:
                         SwitchTo(1);
                         break;
                 }
@@ -167,6 +165,7 @@ public class AuctionMarketFragment extends BaseFragment {
         });
         radioGroup.check(button.getId());
     }
+
     /**
      * 切换
      */
@@ -187,13 +186,14 @@ public class AuctionMarketFragment extends BaseFragment {
                 break;
         }
     }
+
     //接收消息
     @Subscribe(threadMode = ThreadMode.MAIN, sticky = true)
     public void ReciveEventBus(StarBuyActReferralInfo eventBusMessage) {
-        if (eventBusMessage!=null&&!TextUtils.isEmpty(eventBusMessage.getPic_url())){
-            if (TextUtils.isEmpty(pic_url)&&iv_src!=null){
+        if (eventBusMessage != null && !TextUtils.isEmpty(eventBusMessage.getPic_url())) {
+            if (TextUtils.isEmpty(pic_url) && iv_src != null) {
                 pic_url = eventBusMessage.getPic_url();
-                ImageLoaderUtils.display(getActivity(),iv_src, pic_url);
+                ImageLoaderUtils.display(getActivity(), iv_src, pic_url);
             }
         }
     }
@@ -204,10 +204,12 @@ public class AuctionMarketFragment extends BaseFragment {
         EventBus.getDefault().unregister(this);
         super.onDestroy();
     }
+
     @Override
     public void setUserVisibleHint(boolean isVisibleToUser) {
-        LogUtils.loge("setUserVisibleHint>>");
-        if (isVisibleToUser) {
+        if (isVisibleToUser && isVisible() && startSunTime == false) {
+            startSunTime = true;
+            LogUtils.loge("setUserVisibleHint>>vi");
             startRefresh();
         } else {
             stopRefresh();
@@ -224,44 +226,74 @@ public class AuctionMarketFragment extends BaseFragment {
     }
 
     @Override
+    public void onDestroyView() {
+
+        super.onDestroyView();
+    }
+
+    @Override
     public void onPause() {
         LogUtils.loge("停止onPause");
-        stopRefresh();
+        //stopRefresh();
+        setUserVisibleHint(getUserVisibleHint());
         super.onPause();
     }
 
     private void stopRefresh() {
         if (myHandler != null) {
-            myHandler.removeCallbacksAndMessages(myHandler.GRT_DATA);
-            myHandler = null ;
+            startSunTime = false;
+            myHandler.removeCallbacksAndMessages(null);
+            myHandler = null;
             LogUtils.loge("停止刷新stopRefresh");
         }
     }
 
     private void startRefresh() {
+        LogUtils.loge("刷新startRefresh" + myHandler);
         if (myHandler != null) {
             LogUtils.loge("刷新startRefresh");
-            myHandler.removeCallbacksAndMessages(myHandler.GRT_DATA);
+            myHandler.removeCallbacksAndMessages(null);
             initData();
-        }else {
-            myHandler.removeCallbacksAndMessages(myHandler.GRT_DATA);
-            myHandler = new MyHandler1(this);
+        } else {
+            LogUtils.loge("刷新startRefresh,handler=null");
+            myHandler = new Handler() {
+                @Override
+                public void handleMessage(Message msg) {
+                    switch (msg.what) {
+                        case GRT_DATA:
+                            refreshTime();
+                            break;
+                    }
+                }
+            };
+            myHandler.removeCallbacksAndMessages(null);
             initData();
         }
-
     }
 
     private void refreshTime() {
-        if (tv_residue_time!=null&&secondTime>=0&&myHandler!=null){
-            tv_residue_time.setText(TimeUtil.getHourMinuteSecond(secondTime*1000));
+        if (tv_residue_time != null && secondTime >= 0 && myHandler != null && startSunTime) {
+            tv_residue_time.setText(TimeUtil.getHourMinuteSecond(secondTime * 1000));
             secondTime--;
-            myHandler.sendEmptyMessageDelayed(myHandler.GRT_DATA, 1 * 1000);
-        }else if (tv_residue_time!=null&&secondTime<0){
+            myHandler.sendEmptyMessageDelayed(GRT_DATA, 1 * 1000);
+        } else if (tv_residue_time != null && secondTime < 0) {
             tv_residue_time.setText("未开始");
         }
     }
 
-    private static class MyHandler1 extends Handler {
+    private boolean startSunTime = false;
+    final private static int GRT_DATA = 112;
+    private Handler myHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case GRT_DATA:
+                    refreshTime();
+                    break;
+            }
+        }
+    };
+    /*private static class MyHandler1 extends Handler {
         final private static int GRT_DATA = 112;
         private final WeakReference<AuctionMarketFragment> mFragment;
 
@@ -280,6 +312,6 @@ public class AuctionMarketFragment extends BaseFragment {
                 }
             }
         }
-    }
+    }*/
 
 }
