49047d29a64f6c41ef65fe9703502339ec547765
sunlulu <sunlulu@yundian-inc.com>
2017-06-29 20:34:12 +0800

    1,优化钱包详情 2,修复bug

--- app/src/main/java/com/yundian/star/ui/main/adapter/MoneyBagDetailAdapter.java
+++ app/src/main/java/com/yundian/star/ui/main/adapter/MoneyBagDetailAdapter.java
@@ -7,12 +7,16 @@ import com.yundian.star.R;
 import com.yundian.star.base.ListBaseAdapter;
 import com.yundian.star.base.SuperViewHolder;
 import com.yundian.star.been.MoneyDetailListBean;
+import com.yundian.star.greendao.GreenDaoManager;
+import com.yundian.star.greendao.StarInfo;
 import com.yundian.star.utils.BuyHandleStatuUtils;
 import com.yundian.star.utils.TimeUtil;
 
 import java.util.Date;
+import java.util.List;
 
 import static com.yundian.star.R.id.tv_star_name;
+
 
 /**
  * Created by sll on 2017/5/25.
@@ -38,39 +42,30 @@ public class MoneyBagDetailAdapter extends ListBaseAdapter<MoneyDetailListBean> 
         TextView date = holder.getView(R.id.tv_money_detail_date);
         TextView week = holder.getView(R.id.tv_money_detail_week);
 
-        String plus_minus = "+";
+        String plus_minus;
         if (item.getRecharge_type() == 0) {  //充值记录
             plus_minus = "+";
-          name.setText(BuyHandleStatuUtils.getRechargeType(item.getDepositType()));
-        }else if (item.getRecharge_type() == 1){  //约见记录
+            name.setText(BuyHandleStatuUtils.getRechargeType(item.getDepositType()));
+            status.setText(BuyHandleStatuUtils.getRechargeStatus(item.getStatus()));
+        } else {
             plus_minus = "-";
-        }else if (item.getRecharge_type() == 2){  //聊天记录
+            List<StarInfo> starInfos = GreenDaoManager.getInstance().queryLove(item.getTransaction_id());
+            if (starInfos.size() != 0) {
+                StarInfo starInfo = starInfos.get(0);
+                name.setText(String.format(mContext.getString(R.string.name_code), starInfo.getName(), item.getTransaction_id()));
+            }
 
+            if (item.getRecharge_type() == 1) {  //约见记录
+                status.setText("约见");
+            } else if (item.getRecharge_type() == 2) {  //聊天记录
+                status.setText("星聊");
+            }
         }
 
-        name.setText(item.getDepositName());
         week.setText(TimeUtil.getWeekNumber(item.getDepositTime(), TimeUtil.dateFormatYMDHMS));
         Date stringByFormat = TimeUtil.getDateByFormat(item.getDepositTime(), TimeUtil.dateFormatYMDHMS);
         time.setText(TimeUtil.getStringByFormat(stringByFormat, TimeUtil.dateFormatHMS));
-        money.setText(item.getAmount() + "");
+        money.setText(plus_minus + item.getAmount() + "");
         date.setText(TimeUtil.getStringByFormat(stringByFormat, TimeUtil.dateFormatM_D));
-        String statusType = "";
-        switch (item.getStatus()) {
-            case 1:
-                statusType = "处理中";
-                break;
-            case 2:
-                statusType = "成功";
-                break;
-            case 3:
-                statusType = "失败";
-                break;
-            case 4:
-                statusType = "充值取消";
-                break;
-
-        }
-        status.setText(statusType);
-
     }
 }
