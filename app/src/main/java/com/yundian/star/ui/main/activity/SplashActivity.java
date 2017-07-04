be253a42c34d94337f2824e4b1991d63f907c5de
SL-Y <470707973@qq.com>
2017-07-04 09:59:51 +0800

    修改splash暂停方式，避免是动画引起的内存泄露

--- app/src/main/java/com/yundian/star/ui/main/activity/SplashActivity.java
+++ app/src/main/java/com/yundian/star/ui/main/activity/SplashActivity.java
@@ -1,11 +1,8 @@
 package com.yundian.star.ui.main.activity;
 
-import android.animation.Animator;
-import android.animation.AnimatorSet;
-import android.animation.ObjectAnimator;
-import android.animation.PropertyValuesHolder;
+import android.os.Handler;
+import android.os.Message;
 import android.view.MotionEvent;
-import android.view.animation.AccelerateInterpolator;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -28,6 +25,22 @@ public class SplashActivity extends BaseActivity {
     ImageView ivLogo;
     @Bind(R.id.tv_name)
     TextView tvName;
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case 1 :
+                    startNextAct();
+                    break;
+            }
+        }
+
+        private void startNextAct() {
+            startActivity(MainActivity.class);
+            overridePendingTransition(R.anim.act_in_from_right, R.anim.act_out_from_left);
+            finish();
+        }
+    };
 
     @Override
     public int getLayoutId() {
@@ -44,40 +57,41 @@ public class SplashActivity extends BaseActivity {
     //  Bugout.init(this, "1664ea921dcbe122834e440f7f584e2e", "yingyongbao");
     //  initBugOut();
         initGeTui();
-        PropertyValuesHolder alpha = PropertyValuesHolder.ofFloat("alpha", 0.3f, 1f);
-        PropertyValuesHolder scaleX = PropertyValuesHolder.ofFloat("scaleX", 0.3f, 1f);
-        PropertyValuesHolder scaleY = PropertyValuesHolder.ofFloat("scaleY", 0.3f, 1f);
-        ObjectAnimator objectAnimator1 = ObjectAnimator.ofPropertyValuesHolder(tvName, alpha, scaleX, scaleY);
-        ObjectAnimator objectAnimator2 = ObjectAnimator.ofPropertyValuesHolder(ivLogo, alpha, scaleX, scaleY);
-
-        AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.playTogether(objectAnimator1, objectAnimator2);
-        animatorSet.setInterpolator(new AccelerateInterpolator());
-        animatorSet.setDuration(2000);
-        animatorSet.addListener(new Animator.AnimatorListener() {
-            @Override
-            public void onAnimationStart(Animator animator) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animator) {
-                startActivity(MainActivity.class);
-                overridePendingTransition(R.anim.act_in_from_right, R.anim.act_out_from_left);
-                finish();
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animator) {
-
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animator) {
-
-            }
-        });
-        animatorSet.start();
+        mHandler.sendEmptyMessageDelayed(1,2000);
+//        PropertyValuesHolder alpha = PropertyValuesHolder.ofFloat("alpha", 0.3f, 1f);
+//        PropertyValuesHolder scaleX = PropertyValuesHolder.ofFloat("scaleX", 0.3f, 1f);
+//        PropertyValuesHolder scaleY = PropertyValuesHolder.ofFloat("scaleY", 0.3f, 1f);
+//        ObjectAnimator objectAnimator1 = ObjectAnimator.ofPropertyValuesHolder(tvName, alpha, scaleX, scaleY);
+//        ObjectAnimator objectAnimator2 = ObjectAnimator.ofPropertyValuesHolder(ivLogo, alpha, scaleX, scaleY);
+//
+//        AnimatorSet animatorSet = new AnimatorSet();
+//        animatorSet.playTogether(objectAnimator1, objectAnimator2);
+//        animatorSet.setInterpolator(new AccelerateInterpolator());
+//        animatorSet.setDuration(2000);
+//        animatorSet.addListener(new Animator.AnimatorListener() {
+//            @Override
+//            public void onAnimationStart(Animator animator) {
+//
+//            }
+//
+//            @Override
+//            public void onAnimationEnd(Animator animator) {
+//                startActivity(MainActivity.class);
+//                overridePendingTransition(R.anim.act_in_from_right, R.anim.act_out_from_left);
+//                finish();
+//            }
+//
+//            @Override
+//            public void onAnimationCancel(Animator animator) {
+//
+//            }
+//
+//            @Override
+//            public void onAnimationRepeat(Animator animator) {
+//
+//            }
+//        });
+//        animatorSet.start();
     }
 
     private void initBugOut() {
